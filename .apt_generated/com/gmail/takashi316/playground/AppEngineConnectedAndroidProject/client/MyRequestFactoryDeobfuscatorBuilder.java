006371c09bcf2a19a59ae090b2e7a1639f151038
Takashi SASAKI <takashi316@gmail.com>
2012-06-21 14:00:25 +0900

    Client side project created by App Engine Connected Android Project wizard.

---
+++ .apt_generated/com/gmail/takashi316/playground/AppEngineConnectedAndroidProject/client/MyRequestFactoryDeobfuscatorBuilder.java
@@ -0,0 +1,13 @@
+// Automatically Generated -- DO NOT EDIT
+// com.gmail.takashi316.playground.AppEngineConnectedAndroidProject.client.MyRequestFactory
+package com.gmail.takashi316.playground.AppEngineConnectedAndroidProject.client;
+import java.util.Arrays;
+import com.google.web.bindery.requestfactory.vm.impl.OperationData;
+import com.google.web.bindery.requestfactory.vm.impl.OperationKey;
+public final class MyRequestFactoryDeobfuscatorBuilder extends com.google.web.bindery.requestfactory.vm.impl.Deobfuscator.Builder {
+{
+withRawTypeToken("adaSYmTTJXydZhHrH5Wf2ErkS68=", "com.gmail.takashi316.playground.AppEngineConnectedAndroidProject.shared.MessageProxy");
+withRawTypeToken("p95$M_XiUaTQ385fRJjA_AnIUMw=", "com.gmail.takashi316.playground.AppEngineConnectedAndroidProject.shared.RegistrationInfoProxy");
+withRawTypeToken("8KVVbwaaAtl6KgQNlOTsLCp9TIU=", "com.google.web.bindery.requestfactory.shared.ValueProxy");
+withRawTypeToken("FXHD5YU0TiUl3uBaepdkYaowx9k=", "com.google.web.bindery.requestfactory.shared.BaseProxy");
+}}
